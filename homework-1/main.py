abe8a55ad4c8a906a9029bb63e8f312f5e036221
annavaleriev <filenko.a@gmail.com>
2024-01-28 17:26:43 +0300

    Перенесены данные из файла customers_data.csv в таблицу customers

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,19 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
+import psycopg2
+
+connection = psycopg2.connect(host='Localhost', database='north', user='postgres', password='30051980')
+
+try:
+    with connection.cursor() as cursor:
+        with open('north_data/customers_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+
+            for row in reader:
+                cursor.execute("INSERT INTO customers VALUES (%s, %s, %s)",
+                               (row[0], row[1], row[2]))
+    connection.commit()
+
+finally:
+    connection.close()
