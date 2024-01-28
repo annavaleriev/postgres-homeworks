3fdcb0d8ab81ad24482835f74674a35f28d94675
annavaleriev <filenko.a@gmail.com>
2024-01-28 15:03:45 +0300

    SQL-команды для создания таблиц

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,30 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+	customer_id char(5) NOT NULL,
+	company_name varchar(300) NOT NULL,
+	contact_name varchar (300) NOT NULL
+);
+
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(200) NOT NULL,
+	last_name varchar (200) NOT NULL,
+	title varchar (300) NOT NULL,
+	birth_date date,
+	notes text
+);
+
+ALTER TABLE customers
+ADD CONSTRAINT unique_customer_id UNIQUE (customer_id);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id char(5) REFERENCES customers (customer_id) NOT NULL,
+	employee_id int REFERENCES employees (employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar (200) NOT NULL
+);
