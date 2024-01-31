18936e1c0d934e4644d86618ab47b7beae45752a
annavaleriev <filenko.a@gmail.com>
2024-01-31 19:16:12 +0300

    Создана функция def load_fixtures, которая загружает данные сразу в 3 таблицы сразу

--- homework-1/main.py
+++ homework-1/main.py
@@ -3,7 +3,7 @@ import csv
 import psycopg2
 from psycopg2 import sql
 
-connection = psycopg2.connect(host='Localhost', database='north', user='postgres', password='30051980')
+connection = psycopg2.connect(host='localhost', database='north', user='postgres', password='30051980')
 
 
 # try:
@@ -13,35 +13,50 @@ connection = psycopg2.connect(host='Localhost', database='north', user='postgres
 #             next(reader)
 #
 #             for row in reader:
-#                 cursor.execute("INSERT INTO customers VALUES (%s, %s, %s)",
-#                                (row[0], row[1], row[2]))
+#                 cursor.execute("INSERT INTO customers VALUES (%s, %s, %s)", row)
 #     connection.commit()
 #
 # finally:
 #     connection.close()
 
 
-def load_data_to_table(table_name: str, file_path: str):
-    """Загружает данные  из csv в таблицу"""
+def load_fixtures(file_path, table_name):
     with connection.cursor() as cursor:
-        with open(file_path, 'r', newline='', encoding='utf-8') as file:
-            reader = csv.reader(file)
-            next(reader)  # пропускаем 1ю строчку
+        with open(file_path, 'r') as file:
+            reader = csv.DictReader(file)
 
             for row in reader:
-                # подготовка запроса для вставки данных
-                insert_query = sql.SQL("INSERT INTO {} VALUES ({})").format(
-                    sql.Identifier(table_name),
-                    sql.SQL(', ').join(map(sql.Literal, row))
-                )
-                # выполнение запроса
-                cursor.execute(insert_query)
-
-    connection.commit()
+                cursor.execute("INSERT INTO %s VALUES %s" % (table_name, tuple(row.values())))
 
 
-# загрузка данных из csv в таблицы
-load_data_to_table('employees', 'north_data/employees_data.csv')
-load_data_to_table('orders', 'north_data/orders_data.csv')
+load_fixtures('north_data/customers_data.csv', "customers")
+load_fixtures('north_data/employees_data.csv', "employees")
+load_fixtures('north_data/orders_data.csv', "orders")
 
+connection.commit()
 connection.close()
+
+# def load_data_to_table(table_name: str, file_path: str):
+#     """Загружает данные из csv в таблицу"""
+#     with connection.cursor() as cursor:
+#         with open(file_path, 'r', newline='', encoding='utf-8') as file:
+#             reader = csv.reader(file)
+#             next(reader)  # пропускаем 1ю строчку
+#
+#             for row in reader:
+#                 # подготовка запроса для вставки данных
+#                 insert_query = sql.SQL("INSERT INTO {} VALUES ({})").format(
+#                     sql.Identifier(table_name),
+#                     sql.SQL(', ').join(map(sql.Literal, row))
+#                 )
+#                 # выполнение запроса
+#                 cursor.execute(insert_query)
+#
+#     connection.commit()
+#
+#
+# # загрузка данных из csv в таблицы
+# load_data_to_table('employees', 'north_data/employees_data.csv')
+# load_data_to_table('orders', 'north_data/orders_data.csv')
+#
+# connection.close()
