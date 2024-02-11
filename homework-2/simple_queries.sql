f996f846a3017a8e171e8899aeb25b846bfc73c0
annavaleriev <filenko.a@gmail.com>
2024-02-11 17:00:47 +0300

    Прописаны sql запросы

--- homework-2/simple_queries.sql
+++ homework-2/simple_queries.sql
@@ -3,7 +3,6 @@
 SELECT contact_name, city
 FROM customers;
 
-
 -- 2. идентификатор заказа и разницу между датами формирования (order_date) заказа и его отгрузкой (shipped_date) из таблицы orders
 select order_id, shipped_date - order_date as days
 from orders;
